41a2f6a1b18c2418446f828f2bc0bbb2d89c35e9
Deepit162007 <70428754+Deepit162007@users.noreply.github.com>
2020-11-03 23:57:33 +0530

    Add files via upload

---
+++ Crumbled Paper.js
@@ -0,0 +1,23 @@
+class CrumbledPaper{
+constructer (x,y,width,height){
+var options = {
+  isStatic:false,  
+  restitution:0.3,
+  friction:0.5,
+  density:1.2
+
+}
+this.body = Bodies.rectangle(x,y,height,width,options);
+this.width=width;
+this.height=height;
+World.add(world, this.body)
+
+}
+display(){
+var pos =this.body.position;
+rectMode(RIGHT);
+rect(pos.x,pos.y,this.width,this.height);
+
+}
+
+}
